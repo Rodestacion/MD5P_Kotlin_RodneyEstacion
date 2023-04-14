87e505389aec700795491f2000a2b4845a99010a
Rodney Estacion <rodney.estacion@gmail.com>
2023-04-15 07:50:47 +0800

    update new Practice activity

---
+++ src/main/kotlin/PracticeActivity3.kt
@@ -0,0 +1,28 @@
+fun main(){
+    //initialize
+    var item : String = "no item"
+    var priceDay1 : Double = 0.00
+    var priceDay2 : Double = 0.00
+    var priceDay3 : Double = 0.00
+    var average : Double = 0.00
+
+    //input
+    println("Enter item: ")
+    item = readln()
+    println("Enter Price in Day 1: ")
+    priceDay1 = readln().toDouble()
+    println("Enter Price in Day 2: ")
+    priceDay2 = readln().toDouble()
+    println("Enter Price in Day 3: ")
+    priceDay3 = readln().toDouble()
+
+    //process
+    average = (priceDay1 + priceDay2 + priceDay3)/3
+
+    //output
+    println("Item: $item")
+    println("Price in day 1: $priceDay1")
+    println("Price in day 2: $priceDay2")
+    println("Price in day 3: $priceDay3")
+    println("Average price of gasoline is ${String.format("%.2f",average)} per gallon")
+}
