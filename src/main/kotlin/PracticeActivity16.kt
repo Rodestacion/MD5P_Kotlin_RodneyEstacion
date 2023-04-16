572ddaa750774dd60e87be3f63fbd9bdc02daf5a
Rodney Estacion <rodney.estacion@gmail.com>
2023-04-16 23:40:46 +0800

    Add new Practice activity

---
+++ src/main/kotlin/PracticeActivity16.kt
@@ -0,0 +1,33 @@
+fun main(){
+    var number1 : Int = 0
+    var number2 : Int = 0
+    var number3 : Int = 0
+
+    var does1Small : Boolean = false
+    var does2Small : Boolean = false
+    var does3Small : Boolean = false
+
+    print("Enter number 1: ")
+    number1 = readln().toInt()
+    print("Enter number 2: ")
+    number2 = readln().toInt()
+    print("Enter number 3: ")
+    number3 = readln().toInt()
+
+    does1Small = ((number1<=number2)&&(number1<=number3))
+    does2Small = ((number2<=number1)&&(number2<=number3))
+    does3Small = ((number3<=number1)&&(number3<=number2))
+
+
+    if(does1Small&&does2Small&&does3Small){
+        println("The smallest number is $number1")
+        println("All three input number are same.0")
+    }else if(does1Small){
+        println("The smallest number is $number1")
+    }else if(does2Small){
+        println("The smallest number is $number2")
+    }else if(does3Small) {
+        println("The smallest number is $number3")
+    }
+
+}
---
+++ src/main/kotlin/PracticeActivity6.kt
@@ -0,0 +1,19 @@
+fun main(){
+    //initialize
+    var number : Int = 0
+
+    //input
+    println("Enter a number: ")
+    number = readln().toInt()
+
+    //process
+    if(number==0) {
+        println("The number is zero")
+    }else if(number>0) {
+        println("The number is positive")
+    }else if(number<0) {
+        println("The number is negative")
+    }
+    //no need for else
+
+}
