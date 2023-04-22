5b2f35a84d171e69e332a4982ea818bc0769fdd5
Rodney Estacion <rodney.estacion@gmail.com>
2023-04-22 09:23:35 +0800

    Add new activity

---
+++ src/main/kotlin/PracticeActivity20.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var ctr1= 0
+    var number : Int=0
+
+    print("Enter a positive number: ")
+    number = readln().toInt()
+
+    //Display the product multiplied by 1 to 10
+    while(ctr1<10){
+        println("$number * ${ctr1+1} = ${number*(ctr1+1)}")
+        ctr1++
+    }
+}
---
+++ src/main/kotlin/PracticeActivity22.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var ctr1=5
+
+    //Display output
+    //* * * * *
+    //* * * *
+    //* * *
+    //* *
+    //*
+    while(ctr1>0){
+        var ctr2=0
+        while(ctr2<ctr1){
+            print("* ")
+            ctr2++
+        }
+        println()
+        ctr1--
+    }
+}
