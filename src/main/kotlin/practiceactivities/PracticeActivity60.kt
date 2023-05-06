3209fc96beea83bce18a56d65c15847ce953382b
Rodney Estacion <rodney.estacion@gmail.com>
2023-05-06 09:32:18 +0800

    Add new activity

--- src/main/kotlin/practiceactivities/PracticeActivity60.kt
+++ src/main/kotlin/practiceactivities/PracticeActivity60.kt
@@ -4,15 +4,11 @@ fun main(){
     myHigherOrderFunction3(numberList)
 }
 var myStringList= mutableListOf<String>()
-var numberList = {number:Int -> repeat(number){myStringList.add("Number $number")}
+var numberList = {number:MutableList<Int>-> repeat(number.size){myStringList.add("Number ${number.elementAt(it)}")}
     myStringList} //return the value
 
-//var numberList = {vararg number:Int -> repeat(number){myStringList*=(it+1)}
-//    myStringList} //return the value
-
-fun myHigherOrderFunction3 (myFunction: (Int) -> MutableList<String>){
-    //var numbers = listOf<Int>(1,2,3,4,5)
-    println(myFunction(1)) //,2,3,4,5))
-    //println(myFunction(numbers))
+fun myHigherOrderFunction3 (myFunction: (MutableList<Int>) -> MutableList<String>){
+    var numbers = listOf<Int>(1,2,3,4,5)
+    println(myFunction(numbers.toMutableList()))
 
 }
